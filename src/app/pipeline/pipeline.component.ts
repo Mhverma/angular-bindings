16ea2fd800d76a246d852ce8b02a533fbe98f85c
Mhverma <manojkumarhverma@gmail.com>
2019-05-27 22:14:29 +0530

    pipeline demo

---
+++ src/app/pipeline/pipeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pipe-builtins',
+  templateUrl: './pipeline.component.html',
+  styleUrls: ['./pipeline.component.css']
+})
+export class PipelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
